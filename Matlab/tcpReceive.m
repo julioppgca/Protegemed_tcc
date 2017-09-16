054301365bb42bfe5e47743203cc1c78ec70d5ea
julioppgca <julio.ppgca@gmail.com>
2017-09-16 00:19:53 -0300

    Node Js Post Test

---
+++ Matlab/tcpReceive.m
@@ -0,0 +1,25 @@
+%% Matlab script for testing TCP/IP communication with matlab
+%% This script is the receiver part
+%% Written by Philippe Lucidarme
+%% http://www.lucidarme.me
+
+%% TCP/IP Receiver
+
+% Clear console and workspace
+close all;
+clear all;
+clc;
+
+% Configuration and connection
+disp ('Receiver started');
+t=tcpip('192.168.2.110', 1000,'NetworkRole','server');
+
+% Wait for connection
+disp('Waiting for connection');
+fopen(t);
+disp('Connection OK');
+
+% Read data from the socket
+for i=0:10
+    DataReceived=fread(t,2)
+end
